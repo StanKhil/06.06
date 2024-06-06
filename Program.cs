74e89baccf861eadfdff93bacc0335b5cbaed6b0
Stanislav <stankhil@gmail.com>
2024-06-06 19:50:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Привет, я - Станыслав Хиль");
+            Console.WriteLine("Я изучаю программирование уже 2 года, ура-ура!\n");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Я перешел в 11-ИКТ класс РНЛ");
+            Console.WriteLine("Также ч увлекаюсь историей и географией, это конечно кроме программирования и спорта.");
+            Console.WriteLine("Раньше я уже изучал с++, пайтон, sql, VBA");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Мои проекты:\n\t1)Телеграм и дискорд боты на пайтоне\n\t2)Программа для работы с финансами и программа для учета склада на с++");
+            Console.WriteLine("\t3)Макросы для экселя и акцеса\n\t4)Запросы к базам данных на языке Access SQL\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Работа в вашей компании даст мне опыт с возможностью роста, а вам полезного и долгосрочного сотрудника\n\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
